f0d389ccf6093e4bdee3d7ce3eadecfd85ef11af
unknown <prajwalb.xworkz@gmail.com>
2024-06-24 12:46:05 +0530

    this is my first commit

---
+++ Exam.java
@@ -0,0 +1,13 @@
+public class Exam
+{
+	public static void main(String[] args) {
+	    int kannada = 95;
+	    int english = 90;
+	    int hindi = 94;
+	    int maths = 98;
+	    int chemistry = 96;
+	    int biology = 99;
+	    int total = kannada+english+hindi+maths+chemistry+biology;
+		System.out.println("The Total marks is : "+total);
+	}
+}
