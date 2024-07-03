c25e6a35e5e921bc361d84d2988507bd24b06b10
unknown <prajwalb.xworkz@gmail.com>
2024-07-03 10:13:10 +0530

    20-06-24

---
+++ ElectionDetails.java
@@ -0,0 +1,14 @@
+public class ElectionDetails
+{
+	public static byte num = 0;
+	public static short version = 0;
+	public static int rollNum = 0;
+	public static long cNum = 0;
+	public static double average = 0.0;
+	public static float percentage = 0;
+	public static boolean t_or_f = false;
+	public static char grade = 0;
+	public static String name = null ;
+	
+}
+	
---
+++ ElectionDetailsRunner.java
@@ -0,0 +1,16 @@
+public class ElectionDetailsRunner
+{
+	public static void main(String[] args)
+	{
+			System.out.println("Default Details of Datatypes");
+			System.out.println("Default value of byte is:"+ElectionDetails.num);
+			System.out.println("Default value of short is:"+ElectionDetails.version);
+			System.out.println("Default value of int is:"+ElectionDetails.rollNum);
+			System.out.println("Default value of long is:"+ElectionDetails.cNum);
+			System.out.println("Default value of double is:"+ElectionDetails.average);
+			System.out.println("Default value of float is:"+ElectionDetails.percentage);
+			System.out.println("Default value of boolean is:"+ElectionDetails.t_or_f);
+			System.out.println("Default value of char is:"+ElectionDetails.grade);
+			System.out.println("Default value of String is:"+ElectionDetails.name);
+	}
+}
